d2b6fb275f787d8a77011ed52bfcb31d4d376b60
gryphonandrook <38541934+gryphonandrook@users.noreply.github.com>
2021-02-27 14:25:38 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name 'mruby'
+name 'redis'
 maintainer        'Gryphon & Rook Inc'
 license           'All rights reserved'
-description       'Installs ngx_mruby'
+description       'Installs redis'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.1.0'
